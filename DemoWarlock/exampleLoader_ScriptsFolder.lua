905b370e6134558c65b5cd264286b9ef7ac0032a
billchirico <billchirico@gmail.com>
2021-07-27 23:15:17 -0700

    Updates

--- DemoWarlock/exampleLoader_ScriptsFolder.lua
+++ DemoWarlock/exampleLoader_ScriptsFolder.lua
@@ -4,57 +4,58 @@ local AceGUI = Tinkr.Util.AceGUI
 local Config = Tinkr.Util.Config
 local config = Config:New("demo")
 local HTTP = Tinkr.Util.HTTP
-local wowex = {}
+local UI = {}
 local player = "player"
 local target = "target"
 local pet = "pet"
-local authenticated = false
 local name = "Demo Rotation"
 local version = "v1.0-beta"
+
 -- CROMULON --
 
-Tinkr:require("scripts.cromulon.libs.Libdraw.Libs.LibStub.LibStub", wowex)
-Tinkr:require("scripts.cromulon.libs.Libdraw.LibDraw", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.AceGUI30", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-BlizOptionsGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-DropDownGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Frame", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-InlineGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-ScrollFrame", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-SimpleGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TabGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TreeGroup", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Window", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Button", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-CheckBox", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-ColorPicker", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown-Items", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-EditBox", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Heading", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Icon", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-InteractiveLabel", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Keybinding", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Label", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-MultiLineEditBox", wowex)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Slider", wowex)
-Tinkr:require("scripts.cromulon.system.configs", wowex)
-Tinkr:require('scripts.wowex.libs.AceAddon30.AceAddon30' , wowex)
-Tinkr:require('scripts.wowex.libs.AceConsole30.AceConsole30' , wowex)
-Tinkr:require('scripts.wowex.libs.AceDB30.AceDB30' , wowex)
-Tinkr:require('scripts.cromulon.system.storage' , wowex)
-Tinkr:require("scripts.cromulon.libs.libCh0tFqRg.libCh0tFqRg", wowex)
-Tinkr:require("scripts.cromulon.libs.libNekSv2Ip.libNekSv2Ip", wowex)
-Tinkr:require("scripts.cromulon.libs.CallbackHandler10.CallbackHandler10", wowex)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-20", wowex)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-pins-20", wowex)
-Tinkr:require("scripts.cromulon.interface.uibuilder", wowex)
-Tinkr:require("scripts.cromulon.interface.buttons", wowex)
-Tinkr:require("scripts.cromulon.interface.panels", wowex)
-Tinkr:require('scripts.cromulon.interface.minimap' , wowex)
+Tinkr:require("scripts.cromulon.libs.Libdraw.Libs.LibStub.LibStub", UI)
+Tinkr:require("scripts.cromulon.libs.Libdraw.LibDraw", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.AceGUI30", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-BlizOptionsGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-DropDownGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Frame", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-InlineGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-ScrollFrame", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-SimpleGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TabGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TreeGroup", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Window", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Button", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-CheckBox", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-ColorPicker", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown-Items", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-EditBox", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Heading", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Icon", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-InteractiveLabel", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Keybinding", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Label", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-MultiLineEditBox", UI)
+Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Slider", UI)
+Tinkr:require("scripts.cromulon.system.configs", wowUIex)
+Tinkr:require("scripts.wowex.libs.AceAddon30.AceAddon30", UI)
+Tinkr:require("scripts.wowex.libs.AceConsole30.AceConsole30", UI)
+Tinkr:require("scripts.wowex.libs.AceDB30.AceDB30", UI)
+Tinkr:require("scripts.cromulon.system.storage", UI)
+Tinkr:require("scripts.cromulon.libs.libCh0tFqRg.libCh0tFqRg", UI)
+Tinkr:require("scripts.cromulon.libs.libNekSv2Ip.libNekSv2Ip", UI)
+Tinkr:require("scripts.cromulon.libs.CallbackHandler10.CallbackHandler10", UI)
+Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-20", UI)
+Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-pins-20", UI)
+Tinkr:require("scripts.cromulon.interface.uibuilder", UI)
+Tinkr:require("scripts.cromulon.interface.buttons", UI)
+Tinkr:require("scripts.cromulon.interface.panels", UI)
+Tinkr:require("scripts.cromulon.interface.minimap", UI)
 mybuttons.On = false
 mybuttons.Cooldowns = false
 mybuttons.MultiTarget = false
 mybuttons.Interupts = false
 mybuttons.Settings = false
 
+-- END CROMULON --
