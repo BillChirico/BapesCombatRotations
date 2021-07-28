78f3341e51dcf2f07efd7eabcd0a29f775107443
billchirico <billchirico@gmail.com>
2021-07-27 22:19:03 -0700

    Arms updates

--- Warriorbeta/bapes-arms.lua
+++ Warriorbeta/bapes-arms.lua
@@ -60,147 +60,147 @@ mybuttons.Settings = false
 print("|cFFFFD700[Bapes Scripts]|cFF8A2BE2 " .. name .. " " .. version)
 
 Routine:RegisterRoutine(
-  function()
-    if gcd() > latency() then
-      return
-    end
+    function()
+        if gcd() > latency() then
+            return
+        end
 
-    if not latencyCheck() then
-      return
-    end
+        if not latencyCheck() then
+            return
+        end
 
-    -- COMBAT --
-    local function do_combat()
-      -- SETTINGS --
+        -- COMBAT --
+        local function do_combat()
+            -- SETTINGS --
 
-      -- END SETTINGS --
+            -- END SETTINGS --
 
-      -- SPELLS --
+            -- SPELLS --
 
-      local attack = highestrank(88163)
-      local battleShout = highestrank(6673)
+            local attack = highestrank(88163)
+            local battleShout = highestrank(6673)
 
-      local bloodrage = highestrank(2687)
+            local bloodrage = highestrank(2687)
 
-      local deathWish = highestrank(12292)
-      local recklessness = highestrank(1719)
+            local deathWish = highestrank(12292)
+            local recklessness = highestrank(1719)
 
-      local execute = highestrank(5308)
-      local mortalStrike = highestrank(12294)
-      local slam = highestrank(1464)
+            local execute = highestrank(5308)
+            local mortalStrike = highestrank(12294)
+            local slam = highestrank(1464)
 
-      -- END SPELLS --
+            -- END SPELLS --
 
-      -- Auto Attack
-      if UnitExists(target) and not UnitIsDeadOrGhost(target) and enemy(target) then
-        Eval("StartAttack()", "t")
-      end
+            -- Auto Attack
+            if UnitExists(target) and not UnitIsDeadOrGhost(target) and enemy(target) then
+                Eval("StartAttack()", "t")
+            end
 
-      -- HEALING --
+            -- HEALING --
 
-      -- END HEALING --
+            -- END HEALING --
 
-      -- BUFFS --
+            -- BUFFS --
 
-      if not buff(bloodrage, player) and castable(bloodrage, player) then
-        return cast(bloodrage, player)
-      end
+            if not buff(bloodrage, player) and castable(bloodrage, player) then
+                return cast(bloodrage, player)
+            end
 
-      if not buff(deathWish, player) and castable(deathWish, player) then
-        return cast(deathWish, player)
-      end
+            if not buff(deathWish, player) and castable(deathWish, player) then
+                return cast(deathWish, player)
+            end
 
-      if not buff(recklessness, player) and castable(recklessness, player) then
-        return cast(recklessness, player)
-      end
+            if not buff(recklessness, player) and castable(recklessness, player) then
+                return cast(recklessness, player)
+            end
 
-      if not buff(battleShout, player) and castable(battleShout, player) then
-        return cast(battleShout, player)
-      end
+            if not buff(battleShout, player) and castable(battleShout, player) then
+                return cast(battleShout, player)
+            end
 
-      -- END BUFFS --
+            -- END BUFFS --
 
-      -- DEBUFFS --
+            -- DEBUFFS --
 
-      -- END DEBUFFS --
+            -- END DEBUFFS --
 
-      -- ROTATION --
+            -- ROTATION --
 
-      -- Execute
-      if health(target) < 20 and castable(execute, target) then
-        return cast(execute, target)
-      end
+            -- Execute
+            if health(target) < 20 and castable(execute, target) then
+                return cast(execute, target)
+            end
 
-      -- Slam into Mortal Strike
-      if spellisspell(lastspell(), slam) then
-        return cast(mortalStrike, target)
-      end
+            -- Slam into Mortal Strike
+            if spellisspell(lastspell(), slam) then
+                return cast(mortalStrike, target)
+            end
 
-      -- Attack into Slam
-      if spellisspell(lastspell(), attack) then
-        return cast(slam, target)
-      end
+            -- Attack into Slam
+            if spellisspell(lastspell(), attack) then
+                return cast(slam, target)
+            end
 
-      -- END ROTATION --
-    end
+            -- END ROTATION --
+        end
 
-    -- RESTING --
-    local function do_resting()
-      if UnitIsDeadOrGhost(player) or UnitIsDeadOrGhost(target) or IsEatingOrDrinking() then
-        return
-      end
+        -- RESTING --
+        local function do_resting()
+            if UnitIsDeadOrGhost(player) or UnitIsDeadOrGhost(target) or IsEatingOrDrinking() then
+                return
+            end
 
-      -- SETTINGS --
+            -- SETTINGS --
 
-      -- END SETTINGS --
+            -- END SETTINGS --
 
-      -- SPELLS --
+            -- SPELLS --
 
-      local battleShout = highestrank(6673)
+            local battleShout = highestrank(6673)
 
-      -- END SPELLS --
+            -- END SPELLS --
 
-      -- HEALING --
+            -- HEALING --
 
-      -- END HEALING --
+            -- END HEALING --
 
-      -- BUFFS --
+            -- BUFFS --
 
-      if not buff(battleShout, player) and castable(battleShout, player) then
-        return cast(battleShout, player)
-      end
+            if not buff(battleShout, player) and castable(battleShout, player) then
+                return cast(battleShout, player)
+            end
 
-      -- END BUFFS --
-    end
+            -- END BUFFS --
+        end
 
-    if combat(player) then
-      do_combat()
-      return
-    else
-      do_resting()
-      return
-    end
-  end,
-  Routine.Classes.Warrior,
-  "bapes-arms"
+        if combat(player) then
+            do_combat()
+            return
+        else
+            do_resting()
+            return
+        end
+    end,
+    Routine.Classes.Warrior,
+    "bapes-arms"
 )
 Routine:LoadRoutine("bapes-arms")
 
 local bapesArms_settings = {
-  key = "bapes_arms_config",
-  title = "Bapes Scripts",
-  width = 400,
-  height = 400,
-  color = "F58CBA",
-  resize = false,
-  show = false,
-  table = {
-    {
-      key = "heading",
-      type = "heading",
-      text = name .. " " .. version
+    key = "bapes_arms_config",
+    title = "Bapes Scripts",
+    width = 400,
+    height = 400,
+    color = "F58CBA",
+    resize = false,
+    show = false,
+    table = {
+        {
+            key = "heading",
+            type = "heading",
+            text = name .. " " .. version
+        }
     }
-  }
 }
 
 UI.build_rotation_gui(bapesArms_settings)
