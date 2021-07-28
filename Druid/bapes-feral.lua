b148e5f1d5525881abfb1b06e9a149e6ce3b013a
Bill Chirico <billchirico@gmail.com>
2021-07-28 08:28:34 -0700

    Refactoring to use base library

--- Druid/bapes-feral.lua
+++ Druid/bapes-feral.lua
@@ -11,56 +11,57 @@ local target = "target"
 
 -- CROMULON --
 
-Tinkr:require("scripts.cromulon.libs.Libdraw.Libs.LibStub.LibStub", UI)
-Tinkr:require("scripts.cromulon.libs.Libdraw.LibDraw", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.AceGUI30", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-BlizOptionsGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-DropDownGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Frame", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-InlineGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-ScrollFrame", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-SimpleGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TabGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TreeGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Window", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Button", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-CheckBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-ColorPicker", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown-Items", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-EditBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Heading", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Icon", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-InteractiveLabel", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Keybinding", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Label", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-MultiLineEditBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Slider", UI)
-Tinkr:require("scripts.cromulon.system.configs", UI)
-Tinkr:require("scripts.wowex.libs.AceAddon30.AceAddon30", UI)
-Tinkr:require("scripts.wowex.libs.AceConsole30.AceConsole30", UI)
-Tinkr:require("scripts.wowex.libs.AceDB30.AceDB30", UI)
-Tinkr:require("scripts.cromulon.system.storage", UI)
-Tinkr:require("scripts.cromulon.libs.libCh0tFqRg.libCh0tFqRg", UI)
-Tinkr:require("scripts.cromulon.libs.libNekSv2Ip.libNekSv2Ip", UI)
-Tinkr:require("scripts.cromulon.libs.CallbackHandler10.CallbackHandler10", UI)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-20", UI)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-pins-20", UI)
-Tinkr:require("scripts.cromulon.interface.uibuilder", UI)
-Tinkr:require("scripts.cromulon.interface.buttons", UI)
-mybuttons.On = false
-mybuttons.Cooldowns = false
-mybuttons.MultiTarget = false
-mybuttons.Interupts = false
-mybuttons.Settings = false
+-- Tinkr:require("scripts.cromulon.libs.Libdraw.Libs.LibStub.LibStub", UI)
+-- Tinkr:require("scripts.cromulon.libs.Libdraw.LibDraw", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.AceGUI30", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-BlizOptionsGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-DropDownGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Frame", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-InlineGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-ScrollFrame", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-SimpleGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TabGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TreeGroup", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Window", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Button", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-CheckBox", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-ColorPicker", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown-Items", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-EditBox", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Heading", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Icon", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-InteractiveLabel", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Keybinding", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Label", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-MultiLineEditBox", UI)
+-- Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Slider", UI)
+-- Tinkr:require("scripts.cromulon.system.configs", UI)
+-- Tinkr:require("scripts.wowex.libs.AceAddon30.AceAddon30", UI)
+-- Tinkr:require("scripts.wowex.libs.AceConsole30.AceConsole30", UI)
+-- Tinkr:require("scripts.wowex.libs.AceDB30.AceDB30", UI)
+-- Tinkr:require("scripts.cromulon.system.storage", UI)
+-- Tinkr:require("scripts.cromulon.libs.libCh0tFqRg.libCh0tFqRg", UI)
+-- Tinkr:require("scripts.cromulon.libs.libNekSv2Ip.libNekSv2Ip", UI)
+-- Tinkr:require("scripts.cromulon.libs.CallbackHandler10.CallbackHandler10", UI)
+-- Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-20", UI)
+-- Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-pins-20", UI)
+-- Tinkr:require("scripts.cromulon.interface.uibuilder", UI)
+-- Tinkr:require("scripts.cromulon.interface.buttons", UI)
+-- mybuttons.On = false
+-- mybuttons.Cooldowns = false
+-- mybuttons.MultiTarget = false
+-- mybuttons.Interupts = false
+-- mybuttons.Settings = false
+
+Tinkr:require("scripts.cromulon.bapes.shared.imports.lua", UI)
 
 -- END CROMULON --
 
 -- Print name and version
 print("|cFFFFD700[Bapes Scripts]|cFF8A2BE2 " .. name .. " " .. version)
 
-Routine:RegisterRoutine(
-  function()
+Routine:RegisterRoutine(function()
     if gcd() > latency() then
       return
     end
@@ -205,7 +206,7 @@ Routine:RegisterRoutine(
 
       if buff(bearForm, player) then
         -- Frenzied Regeneration
-        if health() <= 35 and castable(frenziedRegeneration, player) then
+        if health() <= 35 and castable(frenziedRegeneration, player) thenwww
           return cast(frenziedRegeneration, player)
         end
 
@@ -369,22 +370,20 @@ Routine:RegisterRoutine(
       end
 
       -- Pounce
-      if UnitExists(target) and spellInRange(pounce) and castable(pounce) then
+      if UnitExists(target) and spellInRange(pounce) and castable(pounce, target) then
         return cast(pounce, target)
       end
     end
 
     if combat(player) then
-      do_combat()
-      return
+        do_combat()
+        return
     else
-      do_resting()
-      return
+        do_resting()
+        return
     end
-  end,
-  Routine.Classes.Druid,
-  "bapes-feral"
-)
+
+end, Routine.Classes.Druid, "bapes-feral")
 Routine:LoadRoutine("bapes-feral")
 
 local bapesFeral_settings = {
--- Hunter/bapes-bm.lua
+++ Hunter/bapes-bm.lua
@@ -12,48 +12,7 @@ local pet = "pet"
 
 -- CROMULON --
 
-Tinkr:require("scripts.cromulon.libs.Libdraw.Libs.LibStub.LibStub", UI)
-Tinkr:require("scripts.cromulon.libs.Libdraw.LibDraw", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.AceGUI30", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-BlizOptionsGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-DropDownGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Frame", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-InlineGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-ScrollFrame", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-SimpleGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TabGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-TreeGroup", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIContainer-Window", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Button", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-CheckBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-ColorPicker", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-DropDown-Items", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-EditBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Heading", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Icon", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-InteractiveLabel", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Keybinding", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Label", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-MultiLineEditBox", UI)
-Tinkr:require("scripts.cromulon.libs.AceGUI30.widgets.AceGUIWidget-Slider", UI)
-Tinkr:require("scripts.cromulon.system.configs", UI)
-Tinkr:require("scripts.wowex.libs.AceAddon30.AceAddon30", UI)
-Tinkr:require("scripts.wowex.libs.AceConsole30.AceConsole30", UI)
-Tinkr:require("scripts.wowex.libs.AceDB30.AceDB30", UI)
-Tinkr:require("scripts.cromulon.system.storage", UI)
-Tinkr:require("scripts.cromulon.libs.libCh0tFqRg.libCh0tFqRg", UI)
-Tinkr:require("scripts.cromulon.libs.libNekSv2Ip.libNekSv2Ip", UI)
-Tinkr:require("scripts.cromulon.libs.CallbackHandler10.CallbackHandler10", UI)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-20", UI)
-Tinkr:require("scripts.cromulon.libs.HereBeDragons.HereBeDragons-pins-20", UI)
-Tinkr:require("scripts.cromulon.interface.uibuilder", UI)
 Tinkr:require("scripts.cromulon.interface.buttons", UI)
-mybuttons.On = false
-mybuttons.Cooldowns = false
-mybuttons.MultiTarget = false
-mybuttons.Interupts = false
-mybuttons.Settings = false
 
 -- END CROMULON --
 
